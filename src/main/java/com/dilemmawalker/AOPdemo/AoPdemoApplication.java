d7f4a05a324b46b10482bf662b9f5c4c911bf856
Yash Handa <yashhanda500@gmail.com>
2025-04-15 16:53:26 +0530

    first commit

---
+++ src/main/java/com/dilemmawalker/AOPdemo/AoPdemoApplication.java
@@ -0,0 +1,13 @@
+package com.dilemmawalker.AOPdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoPdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AoPdemoApplication.class, args);
+	}
+
+}
